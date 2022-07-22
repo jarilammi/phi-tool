ce1502396552ebfe4f3a7b52c266d6efda296509
Jari Lammi <1969656+jarilammi@users.noreply.github.com>
2022-07-22 14:12:21 +0300

    Reads command-line arguments.

--- PhiTool.java
+++ PhiTool.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Stack;
 public class PhiTool {
 
-  protected static double phi=(1 + Math.sqrt(5))/2;
+  protected final static double phi=(1 + Math.sqrt(5))/2;
   protected static double[] part = { };
 
   protected static class PhiCut {
@@ -10,25 +12,47 @@ public class PhiTool {
     Stack <Double> parts = new Stack <Double>();
 
     private PhiCut(double maxvalue) {
-      double volume = maxvalue/phi;
-      for (int i=0; i<10; i++) {
+      double volume = maxvalue;
+      for (int i=0; i<11; i++) {
         this.parts.push(volume);
         volume /= phi;
       }
     }
 
-    public double pop() {
+    private double pop() {
       return this.parts.pop();
+    }
+
+    private boolean empty() {
+      return this.parts.empty();
+    }
+
+    public String toString() {
+      String phistack = "";
+      ListIterator<Double> ListIterator = this.parts.listIterator(this.parts.size());
+      while (ListIterator.hasPrevious()) {
+        phistack += (ListIterator.previous() + "\n");
+      }
+      return phistack.trim();
     }
   }
 
   public static void main(String[] args) {
-    PhiCut ratios = new PhiCut(100);
-    double ratio=0.0;
-    while(ratio<61) {
-      ratio=ratios.pop();
-      System.out.println(ratio);
+    PhiCut ratios;
+    if(args.length == 1 && isNumeric(args[0])) {
+      ratios = new PhiCut(Double.parseDouble(args[0]));
+    } else {
+      ratios = new PhiCut(100);
     }
-    System.out.println("100");
+    System.out.println(ratios);
+  }
+
+  private static boolean isNumeric(String str) {
+    try {
+      Double.parseDouble(str);
+      return true;
+    } catch(NumberFormatException e) {
+      return false;
+    }
   }
 }
